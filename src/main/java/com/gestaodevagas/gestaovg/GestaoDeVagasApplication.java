495a50b5c54b94a119be79f1aba988c6f64c1d74
Matheus Davi <mahteus2008@gmail.com>
2024-04-26 19:50:24 -0300

    first commit

---
+++ src/main/java/com/gestaodevagas/gestaovg/GestaoDeVagasApplication.java
@@ -0,0 +1,13 @@
+package com.gestaodevagas.gestaovg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaodevagas/gestaovg/GestaoDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaodevagas.gestaovg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
